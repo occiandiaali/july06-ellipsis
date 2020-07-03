fca72e921dcb3b06b22eb7381bbbafd1afe386da
occiandiaali <ocean.diaali@gmail.com>
2020-07-03 16:35:25 +0100

    initialize service to fetch data

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  articles;
+  constructor(private newsService: NewsService) {}
 
-}
+  ngOnInit() {
+    this.newsService.getNews().subscribe((data) => {
+      console.log(data);
+      this.articles = data['articles'];
+    });
+  }
+
+} // class
