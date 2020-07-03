7d840528ac5951d0d2cc7c30bcefe90842fa1715
occiandiaali <ocean.diaali@gmail.com>
2020-07-03 16:30:08 +0100

    initial commit

---
+++ src/app/news.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  apiUrl = 'https://www.reddit.com/r/worldnews/top/?t=hour';
+  apiKey = '40f233687a574369b42c8cd177ab60e0';
+
+  constructor(private httpClient: HttpClient) { 
+    this.getNews();
+  }
+
+  getNews() {
+    return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.apiKey}`);
+  }
+} // class
